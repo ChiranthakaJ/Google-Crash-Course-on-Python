0e1943a728eff1912a4240c0800f273f3314894f
chiranthakaj <chiransj@gmail.com>
2020-08-06 18:03:50 +0530

    This is the first commit.

---
+++ Python_Functions_With_Para.py
@@ -0,0 +1,31 @@
+#Example 01 - Function with Multiple Parameters Type 01. 
+
+def get_seconds(hours, minutes, seconds):
+    return 3600*hours + 60*minutes + seconds
+
+amount_a = get_seconds(2,30,0)
+amount_b = get_seconds(0, 45, 15)
+result = amount_a+ amount_b
+
+print(result)
+
+#Example 02 - Function with Multiple Parameters Type 02. 
+
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds - hours * 3600) // 60
+    remaining_seconds = seconds - hours * 3600 - minutes * 60
+    return hours, minutes, remaining_seconds
+
+hours, minutes, remaining_seconds = convert_seconds(5000) #In here we assign the results of the function to 3 different variables at once
+
+print(hours, minutes, remaining_seconds) 
+
+#Similar to above assign statement, you can use the standard method like the below.
+
+hours = convert_seconds(5000)
+minutes = convert_seconds(5000)
+remaining_seconds = convert_seconds(5000)
+
+print(hours, minutes, remaining_seconds)
+
