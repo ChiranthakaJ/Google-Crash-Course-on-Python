5fb390e22d45708bfe6423ed808644a481fca845
chiranthakaj <chiransj@gmail.com>
2020-08-08 17:57:17 +0530

    Forth commit

--- Test01.py
+++ Test01.py
@@ -48,5 +48,5 @@ def lucky_number(name):
     number = len(name) * 9
     return ("Hello " + name + ". Your lucky number is " + str(number))
 	    
-print(lucky_number("Kay"))
-print(lucky_number("Cameron"))
+print(lucky_number("Chiranthaka"))
+print(lucky_number("Gayathri"))
