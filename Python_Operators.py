d166733b22c1e612fb33ca88f3198b8efd4ad6fe
chiranthakaj <chiransj@gmail.com>
2020-08-08 18:29:21 +0530

    Second commit

--- Python_Operators.py
+++ Python_Operators.py
@@ -9,3 +9,13 @@ print("Yellow" > "Brown" and "Magenta" > "Cyan")
 #Reason is Y comes after B and M comes after C.
 
 #The operators checked the strings against the alphabetical order of the English language.
+
+print(25 >50 or 1 != 2)
+
+#Result ==> True
+#Reason is either one of the logics are correct. In here 1 is not equal to 2 and is correct. But 25 is not larger than 50 and it's false. Since one of the two logics and correct the result is 'True'.
+
+#The answer to be 'True' either if the logics or all the logics should be true. If none of them were true the answer is false.
+
+
+#The not operator simply inverts the value of the statement immediately following it. So if a statement evaluates to True, and we put the not operator in front of it, it would become False.
