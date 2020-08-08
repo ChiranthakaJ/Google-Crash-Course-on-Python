69f2b31ce7eaa423ca81cc23de24c303f2602190
chiranthakaj <chiransj@gmail.com>
2020-08-08 17:47:40 +0530

    Forth commit

--- Test01.py
+++ Test01.py
@@ -30,3 +30,23 @@ my_trip_km = convert_distance(55)
 print("The distance in kilometers is " + str(my_trip_km))
 
 print("The round-trip in kilometers is " + str(my_trip_km*2))
+
+
+#THIS FUNCTION COMPARES TWO NUMBERS AND RETURNS THEM
+def order_numbers(number1, number2):
+    if number2 > number1:
+        return number1, number2
+    else:
+        return number2, number1
+
+smaller, bigger = order_numbers(100, 99)
+print(smaller, bigger)
+
+
+#THIS FUNCTION CALCULATE THE LUCKY NUMBER BY USING THE NAME
+def lucky_number(name):
+    number = len(name) * 9
+    return ("Hello " + name + ". Your lucky number is " + str(number))
+	    
+print(lucky_number("Kay"))
+print(lucky_number("Cameron"))
