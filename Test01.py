2bc840a1193bce5bddb65626f31e166a22edf762
chiranthakaj <chiransj@gmail.com>
2020-08-08 21:46:50 +0530

    Fifth commit

--- Test01.py
+++ Test01.py
@@ -50,3 +50,26 @@ def lucky_number(name):
 	    
 print(lucky_number("Chiranthaka"))
 print(lucky_number("Gayathri"))
+
+
+#THIS FUNCTION concatinate 2 string variables depending their availability.
+
+def format_name(first_name, last_name):
+	# code goes here
+	if first_name !="" and last_name !="":
+	  string = ("Name: "+ last_name +", "+ first_name)
+	elif first_name !=" " or last_name !=" ":
+	  string = ("Name: "+ last_name + first_name)
+	return string 
+
+print(format_name("Ernest", "Hemingway"))
+# Should return the string "Name: Hemingway, Ernest"
+
+print(format_name("", "Madonna"))
+# Should return the string "Name: Madonna"
+
+print(format_name("Voltaire", ""))
+# Should return the string "Name: Voltaire"
+
+print(format_name("", ""))
+# Should return an empty string
