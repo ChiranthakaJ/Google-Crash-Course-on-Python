54e4f95fd9ef66e2b7ca82dbb30f3b78845f23b3
chiranthakaj <chiransj@gmail.com>
2020-08-09 00:28:18 +0530

    Ninth commit

--- Python_If_Statement.py
+++ Python_If_Statement.py
@@ -200,4 +200,4 @@ print(fractional_part(5, 4)) # Should be 0.25
 print(fractional_part(5, 3)) # Should be 0.66...
 print(fractional_part(5, 2)) # Should be 0.5
 print(fractional_part(5, 0)) # Should be 0
-print(fractional_part(0, 5)) # Should be 0
+print(fractional_part(0, 5)) # Should be 0 
