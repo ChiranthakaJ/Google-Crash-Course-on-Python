d9ba6ecbfc56f6fc077f2a8ed854e88bc3cf976b
chiranthakaj <chiransj@gmail.com>
2020-08-08 18:06:18 +0530

    First commit

---
+++ Python_Operators.py
@@ -0,0 +1,11 @@
+print("Yellow" > "Cyan" and "Brown" > "Magenta")
+
+#Result ==> False
+#Reason is Y comes after C and B not comes after M. 
+
+print("Yellow" > "Brown" and "Magenta" > "Cyan") 
+
+#Result ==> True
+#Reason is Y comes after B and M comes after C.
+
+#The operators checked the strings against the alphabetical order of the English language.
