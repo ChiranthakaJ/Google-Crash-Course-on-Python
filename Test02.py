b57f7f35397d0ee813e2369c5808085dff675cfe
chiranthakaj <chiransj@gmail.com>
2020-08-24 21:39:50 +0530

    Second commit

--- Test02.py
+++ Test02.py
@@ -8,3 +8,11 @@ def initials(phrase):
 print(initials("Universal Serial Bus")) # Should be: USB
 print(initials("local area network")) # Should be: LAN
 print(initials("Operating system")) # Should be: OS
+
+
+def student_grade(name, grade):
+     return ("{name} received {grade}% on the exam".format(name=name, grade=grade))
+
+print(student_grade("Reed", 80))
+print(student_grade("Paige", 92))
+print(student_grade("Jesse", 85))
