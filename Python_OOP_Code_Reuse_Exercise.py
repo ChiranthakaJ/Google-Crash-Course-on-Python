9e3c8632acd04eaf171d34bdd687a3f19274a52e
chiranthakaj <chiransj@gmail.com>
2020-09-27 10:06:18 +0530

    Second commit

--- Python_OOP_Code_Reuse_Exercise.py
+++ Python_OOP_Code_Reuse_Exercise.py
@@ -118,3 +118,66 @@ print(server.load())
 #Go back to the Server class definition and fill in the missing parts for the add_connection and load methods to make the cell above print a number different than zero. As the load is calculated randomly, this number should be different each time the code is executed.
 
 #Hint: Recall that you can iterate through the values of your connections dictionary just as you would any sequence.
+
+#Great! If your output is a random number between 1 and 10, you have successfully coded the add_connection and load methods of the Server class. Well done!
+
+#What about closing a connection? Right now the close_connection method doesn't do anything. Go back to the Server class definition and fill in the missing code for the close_connection method to make the following code work correctly:
+
+server.close_connection("192.168.1.1")
+print(server.load())
+
+'''
+0
+'''
+
+#You have successfully coded the close_connection method if the cell above prints 0.
+
+#Hint: Remember that del dictionary[key] removes the item with key key from the dictionary.
+
+#Alright, we now have a basic implementation of the server class. Let's look at the basic LoadBalancing class. This class will start with only one server available. When a connection gets added, it will randomly select a server to serve that connection, and then pass on the connection to the server. The LoadBalancing class also needs to keep track of the ongoing connections to be able to close them. This is the basic structure:
+
+##Begin Portion 2#
+class LoadBalancing:
+    def __init__(self):
+        """Initialize the load balancing system with one server"""
+        self.connections = {}
+        self.servers = [Server()]
+
+    def add_connection(self, connection_id):
+        """Randomly selects a server and adds a connection to it."""
+        server = random.choice(self.servers)
+        # Add the connection to the dictionary with the selected server
+        # Add the connection to the server
+        server.add_connection(connection_id)
+        self.ensure_availability()
+
+    def close_connection(self, connection_id):
+        """Closes the connection on the the server corresponding to connection_id."""
+        # Find out the right server
+        # Close the connection on the server
+        # Remove the connection from the load balancer
+        for server in self.servers:
+            if connection_id in server.connections:
+                server.close_connection(connection_id)
+                break
+           
+    def avg_load(self):
+        """Calculates the average load of all servers"""
+        # Sum the load of each server and divide by the amount of servers
+        total_load = 0
+        total_server = 0
+        for server in self.servers:
+            total_load += server.load()
+            total_server += 1
+        return total_load/total_server
+
+    def ensure_availability(self):
+        """If the average load is higher than 50, spin up a new server"""
+        if self.avg_load() > 50:
+            self.servers.append(Server())
+
+    def __str__(self):
+        """Returns a string with the load for each server."""
+        loads = [str(server) for server in self.servers]
+        return "[{}]".format(",".join(loads))
+#End Portion 2#
